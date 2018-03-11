ff023f53c6b0c08ab069916c1317144dfbea88dc
Ahmed Sherif <e.ahmedsherif@gmail.com>
2018-03-11 21:35:16 +0300

    updates

--- Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data-preprocessing-template.py
+++ Part 1 - Data Preprocessing/Section 2 -------------------- Part 1 - Data Preprocessing --------------------/data-preprocessing-template.py
@@ -33,7 +33,7 @@ y = labelEncoder_y.fit_transform(y)
 """
 
 
-# Spreading the dataset into Training set and Data set
+# Splitting the dataset into Training set and Data set
 from sklearn.cross_validation import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
---
+++ Part 2 - Regression/Section 6 - Polynomial Regression/polynomial_regression.py
@@ -0,0 +1,66 @@
+# Polynomial Regression
+
+# importing the libraries
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas as pd
+
+# importing the data
+dataset = pd.read_csv("Position_Salaries.csv")
+# The upper bound '2' is ignored, [:,1:2] is used to generate a matrix instead of a vector in case you write [:,1]
+X = dataset.iloc[:,1:2].values
+y = dataset.iloc[:,2].values
+
+ 
+
+
+# Splitting the dataset into Training set and Data set
+"""from sklearn.cross_validation import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)"""
+
+
+"""
+# Feature Scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)               # no fit for the test data
+"""
+
+# Fitting Linear Regression to the dataset
+from sklearn.linear_model import LinearRegression
+lin_reg = LinearRegression() 
+lin_reg.fit(X,y)
+
+# Fitting Polynomial Regression to the dataset
+from sklearn.preprocessing import PolynomialFeatures
+poly_reg = PolynomialFeatures(degree = 4)
+X_poly = poly_reg.fit_transform(X)
+lin_reg_2 = LinearRegression()
+lin_reg_2.fit(X_poly, y)
+
+
+# Visualizing the linear regression results
+plt.scatter(X, y , color='red')
+plt.plot(X, lin_reg.predict(X), color='blue')
+plt.title('Truth or Bluff (Linear Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+# Visualizing the polynomial regression results
+X_grid = np.arange(min(X), max(X), 0.1)
+X_grid = X_grid.reshape((len(X_grid), 1))
+plt.scatter(X, y , color='red')
+plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color='blue')
+plt.title('Truth or Bluff (polynomial Regression)')
+plt.xlabel('Position Level')
+plt.ylabel('Salary')
+plt.show()
+
+
+# Predicting a new result using Linear Regression
+lin_reg.predict(6.5)
+
+# Predicting a new result using polynomial Regression
+lin_reg_2.predict(poly_reg.fit_transform(6.5))
